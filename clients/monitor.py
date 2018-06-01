db635960c60e43a2a5b79378893f12a6d449af03
Ricardo Cardenes <rcardenes@gemini.edu>
2018-05-31 14:42:04 -1000

    First iteration of the client

---
+++ clients/monitor.py
@@ -0,0 +1,224 @@
+#!/usr/bin/env python
+
+# vim: ai:sw=4:sts=4:expandtab
+
+from contextlib import contextmanager
+from collections import defaultdict, namedtuple
+from time import sleep
+import ctypes
+
+import epics
+from epics import PV
+import numpy as np
+try:
+    from numpy import datetime64, timedelta64
+    def getdt(sec, nsec):
+        return np.datetime64('{0}.{1}'.format(sec, nsec), 'ns')
+except ImportError:
+    from datetime import datetime, timedelta
+    def getdt(sec, nsec):
+        return datetime.utcfromtimestamp(sec + nsec / 1000000000.)
+    def getdelta(mdelta):
+        return timedelta(microseconds=mdelta)
+    def isodt(dt):
+        return dt.isoformat()
+
+import numpy as np
+
+MONITORED_OUTPUTS = {
+    'VALA': 'Ticks per second',
+    'VALB': 'Timestamp: Seconds',
+    'VALC': 'Timestamp: Nanoseconds',
+    'VALD': 'Number of events',
+    'VALE': 'Index of first event',
+    'VALF': 'Chunk #1',
+    'VALG': 'Chunk #2',
+    'VALH': 'Chunk #3',
+    'VALI': 'Chunk #4',
+    'VALJ': 'Chunk #5',
+    'VALK': 'Chunk #6',
+    'VALL': 'Chunk #7',
+    'VALM': 'Chunk #8',
+    'VALT': 'Ticks at the time of timestamp',
+    'VALU': 'Record size (in uint32_t)'
+    }
+
+CHUNKSUFFS = "FGHIJKLM"
+CHUNKS = set('VAL{0}'.format(x) for x in CHUNKSUFFS)
+
+epics.ca.HAS_NUMPY = False
+
+MAX_EVENTS = 4096
+
+# NOTE: This list is valid for RTEMS 4.10. It may change across versions
+# There's also STATES_READY = 0x0000, but we treat that one in a special way
+RtemsState = namedtuple("RtemsState", "mask text")
+RTEMS_STATES = [
+    RtemsState(0x00001, "DORMANT"),
+    RtemsState(0x00002, "SUSPENDED"),
+    RtemsState(0x00004, "TRANSIENT"),
+    RtemsState(0x00008, "DELAYING"),
+    RtemsState(0x00010, "WAITING FOR TIME"),
+    RtemsState(0x00020, "WAITING FOR BUFFER"),
+    RtemsState(0x00040, "WAITING FOR SEGMENT"),
+    RtemsState(0x00080, "WAITING FOR MESSAGE"),
+    RtemsState(0x00100, "WAITING FOR EVENT"),
+    RtemsState(0x00200, "WAITING FOR SEMAPHORE"),
+    RtemsState(0x00400, "WAITING FOR MUTEX"),
+    RtemsState(0x00800, "WAITING FOR CONDITION VARIABLE"),
+    RtemsState(0x01000, "WAITING FOR JOIN AT EXIT"),
+    RtemsState(0x02000, "WAITING FOR RPC REPLY"),
+    RtemsState(0x04000, "WAITING FOR PERIOD"),
+    RtemsState(0x08000, "WAITING FOR SIGNAL"),
+    RtemsState(0x10000, "WAITING FOR BARRIER"),
+    RtemsState(0x20000, "WAITING FOR RW LOCK")
+]
+rtems_states_map = dict(RTEMS_STATES)
+status_masks = np.array([state.mask for state in RTEMS_STATES], dtype=np.uint32)
+
+class RtemsStatsEvent(ctypes.Structure):
+    _fields_ = [("type", ctypes.c_uint32),
+                ("state", ctypes.c_uint32),
+                ("begin_id", ctypes.c_uint32),
+                ("end_id", ctypes.c_uint32),
+                ("ticks", ctypes.c_uint32)]
+
+    def status_text(self):
+        bits = status_masks[status_masks & self.state != 0]
+        return "READY" if len(bits) == 0 else (', '.join(rtems_states_map[mask] for mask in bits))
+
+class EventPrinter(object):
+    def __init__(self, tick_rate_ms = 100): # 10000 ticks/s
+        self.trate = getdelta(tick_rate_ms)
+        self.last_timestamp = None
+        self.tstamp_ticks = None
+        self.last_tick = None
+        self.sub_tick = 0
+
+    def set_timestamp(self, tstamp, ticks):
+        self.last_timestamp = tstamp
+        self.tstamp_ticks = ticks
+
+    def print_ev(self, event):
+        ticks_since = event.ticks
+        tstamp = self.last_timestamp + self.trate * (ticks_since - self.tstamp_ticks)
+
+        if self.last_tick == ticks_since:
+            self.sub_tick += 1
+        else:
+            self.sub_tick = 0
+
+        self.last_tick = ticks_since
+        print "{stamp}: {id_a:#08x} -> {id_b:#08x} ({state})".format(
+                id_a  = event.begin_id,
+                id_b  = event.end_id,
+                stamp = isodt(tstamp),
+                state = event.status_text()
+                )
+
+class Buffer(object):
+    def __init__(self):
+        self.seq_no = None
+        self.attributes = dict((x, None) for x in MONITORED_OUTPUTS)
+        self._set_attributes = set()
+        self.invalid = False
+
+    def set_data(self, key, value):
+        output = key.split('.')[-1]
+        self.attributes[output] = value
+        self._set_attributes.add(output)
+
+    def done(self):
+        return self._set_attributes == set(MONITORED_OUTPUTS)
+
+    @property
+    def timestamp(self):
+        # Needs support from NumPy... only >= 1.7
+        return getdt(self.attributes['VALB'], self.attributes['VALC'])
+
+    @property
+    def ticks_at_timestamp(self):
+        return self.attributes['VALT']
+
+    @property
+    def number_of_events(self):
+        return self.attributes['VALD']
+
+    @property
+    def longs_per_entry(self):
+        return self.attributes['VALU']
+
+    def dump(self, printer):
+        events = self.number_of_events
+        copyevents = self.number_of_events
+        if events > 0:
+            print "#events: {0}".format(events)
+            # Size of each structure in bytes
+            sizeof = self.longs_per_entry * 4
+            data = (RtemsStatsEvent * events)()
+            for offset, source in ((n * (512 * sizeof), self.attributes['VAL{0}'.format(x)]) for (n, x) in enumerate(CHUNKSUFFS)):
+                block_size = (events if events < 512 else 512) * sizeof
+                ctypes.memmove(ctypes.byref(data, offset), source, block_size)
+                events -= 512
+                if events < 1:
+                    break
+            printer.set_timestamp(self.timestamp, self.ticks_at_timestamp)
+            for n in range(copyevents):
+                printer.print_ev(data[n])
+
+class SessionTracker(object):
+    def __init__(self, pvprefix, printer):
+        self.prefix = pvprefix
+        self.control = "{0}:control".format(pvprefix)
+        self.main    = PV("{0}:export".format(pvprefix))
+        self.outputs = [PV('{0}:export.{1}'.format(pvprefix, var), auto_monitor=epics.dbr.DBE_VALUE, callback=self.callback)
+                        for var in MONITORED_OUTPUTS]
+        self.buffers = defaultdict(Buffer)
+        self.printer = printer
+
+    def callback(self, pvname, value, count, status, timestamp, **kw):
+        if status != 0:
+            return
+        try:
+            buff = self.buffers[timestamp]
+            buff.set_data(pvname, value)
+            if buff.done():
+                print "Dumping dataset with timestamp {0}, reported start at {1}".format(timestamp, buff.timestamp)
+                buff.dump(self.printer)
+        except KeyError:
+            print "Setting {0} as invalid".format(timestamp)
+            buff.invalid = True
+
+    def enable(self, en):
+        print "ENABLING" if en else "DISABLING"
+        epics.PV("{0}.A".format(self.control)).put('ENABLE' if en else 'DISABLE')
+        epics.PV("{0}.PROC".format(self.control)).put(1)
+
+@contextmanager
+def monitor_session(monitored):
+    print "Creating the SessionTracker for PV: {0}".format(monitored)
+    mon = SessionTracker(monitored, EventPrinter())
+    try:
+        print "Yielding..."
+        yield mon
+    finally:
+        mon.enable(False)
+
+def main(top):
+    try:
+        with monitor_session("{top}rtems:stats".format(top=top)) as mon:
+            mon.enable(True)
+            while True:
+                sleep(1)
+    except KeyboardInterrupt:
+        pass
+
+if __name__=='__main__':
+    import sys
+    try:
+        TOP = sys.argv[1]
+    except IndexError:
+        print "Pass the value for ${top}"
+        sys.exit(1)
+
+    main(TOP)
